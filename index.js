c0a0c4f8fab8f5f63facf3dfb00228d7fad6bbc6
Ronaldo <ronaldo.carvalho@hotmail.com>
2022-05-13 16:37:22 -0300

    correção node

--- index.js
+++ index.js
@@ -1,20 +1,10 @@
-const express = require("express");
-const app = express();
-const path = require("path");
-const router = express.Router();
+const express = require('express')
+const path = require('path')
+const app = express()
+const router = express.Router()
 
-/* Routers */
+app.use(express.static(path.join(__dirname, 'views')))
 
-router.get('/', function(req,res){
-    res.sendFile(path.join(__dirname+'/views/index.html'))
-});
-/* Home */
-// app.get('/', function(req,res){
-//     res.sendFile(__dirname + '/views/index.html');
-// });
-
-
-app.use('/',router);
-app.listen(process.env.port || 8081, ()=>{
-    console.log("Server on 8081");
-});
+app.listen(process.env.port || 8081, () => {
+  console.log('Server on 8081')
+})
